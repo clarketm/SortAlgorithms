23ee750bff433706ae2644567902e0e3288f328c
Travis Clarke <travis.m.clarke@gmail.com>
2016-12-10 21:23:32 -0800

    Make modular as well as patch "quicksort" and "mergesort".

--- sort.js
+++ sort.js
@@ -1,235 +1,180 @@
-(function() {
-    'use strict';
+;(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['exports'], factory);
+    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+        factory(exports);
+    } else {
+        factory(root);
+    }
+}(this, function (exports) {
+        'use strict';
 
-    const utils = {
-        assertString: function(string) {
-            const isString = typeof string === 'string';
-            if (!isString) throw new Error(`Parameter needs to be a type of string: ${string}`);
-        },
+        const utils = {
+            assertString: function (string) {
+                const isString = typeof string === 'string';
+                if (!isString) throw new Error(`Parameter needs to be a type of string: ${string}`);
+            },
 
-        assertArray: function(array) {
-            const notArray = !(array instanceof Array);
-            if (notArray) throw new Error(`Parameter needs to be an instance of Array: ${array}`);
-        }
-    };
-
-    function SortAlgorithms(visualizer) {
-        const sortingAlgorithms = {
-            insertion: insertionSort,
-            selection: selectionSort,
-            bubble: bubbleSort,
-            merge: mergeSort,
-            quick: quickSort
+            assertArray: function (array) {
+                const isArray = array instanceof Array;
+                if (!isArray) throw new Error(`Parameter needs to be an instance of Array: ${array}`);
+            }
         };
 
-        let counter = 0;
+        let SortAlgorithms = function (visualizer) {
 
-        function insertionSort(array) {
-            array = array.slice();
-            if (array.length <= 1) return;
+            const sortingAlgorithms = {
+                insertion: insertionSort,
+                selection: selectionSort,
+                bubble: bubbleSort,
+                merge: mergeSort,
+                quick: quickSort
+            };
 
-            let ptr = 1;
-            let p1 = 1;
-            let p2 = 0;
+            let counter = 0;
 
-            while (ptr < array.length) {
-                let key = array[ptr];
-                while (p2 >= 0 && key < array[p2]) {
-                    array[p1] = array[p2];
-                    array[p2] = key;
-                    p1--;
-                    p2--;
-                    visualizer(array.slice(), ++counter, 'insertion');
-                }
-                ptr++;
-                p1 = ptr;
-                p2 = p1 - 1;
-            }
-            return array;
-        }
+            function insertionSort(array) {
+                array = array.slice();
+                if (array.length <= 1) return;
 
-        function selectionSort(array) {
-            array = array.slice();
-            let tmp;
-            for (let i = 0; i < array.length - 1; i++) {
-                for (let j = i + 1; j < array.length; j++) {
-                    if (array[j] < array[i]) {
-                        // swap
-                        tmp = array[i];
-                        array[i] = array[j];
-                        array[j] = tmp;
-                        visualizer(array.slice(), ++counter, 'selection');
-                    }
-                }
-            }
-            return array;
-        }
-
-        function bubbleSort(array) {
-            array = array.slice()
-            let isSorted = false;
-            let stoppingPoint = array.length;
-            while (!isSorted) {
-                isSorted = true;
-                for (let current = 0, next = 1; next < stoppingPoint;) {
-                    if (array[current] > array[next]) {
-                        isSorted = false;
-                        let tmp = array[current];
-                        array[current] = array[next];
-                        array[next] = tmp;
-                        visualizer(array.slice(), ++counter, 'bubble');
-                    }
-                    current++;
-                    next++;
-                }
-                stoppingPoint--;
-            }
-            return array;
-        }
-
-        function mergeSort(array) {
-            return divide(leftHalf(array), rightHalf(array));
-
-            function divide(left, right) {
-                if (left.length <= 1 && right.length <= 1) {
-                    return conquer(left, right);
-                }
-
-                return conquer(
-                    divide(leftHalf(left), rightHalf(left)),
-                    divide(leftHalf(right), rightHalf(right))
-                );
-            }
-
-            function conquer(a, b) {
-                let p1 = 0;
+                let ptr = 1;
+                let p1 = 1;
                 let p2 = 0;
 
-                let result = [];
-                let i = 0;
-
-                while (p1 < a.length && p2 < b.length) {
-                    if (a[p1] <= b[p2]) {
-                        result.push(a[p1]);
-                        p1++;
-                    } else {
-                        result.push(b[p2]);
-                        p2++;
+                while (ptr < array.length) {
+                    let key = array[ptr];
+                    while (p2 >= 0 && key < array[p2]) {
+                        array[p1] = array[p2];
+                        array[p2] = key;
+                        p1--;
+                        p2--;
+                        visualizer(array.slice(), ++counter, 'insertion');
                     }
-                    i++;
-                }
-
-                while (p1 < a.length) {
-                    result.push(a[p1]);
-                    i++;
-                    p1++;
-                }
-
-                while (p2 < b.length) {
-                    result.push(b[p2]);
-                    i++;
-                    p2++;
-                }
-
-                visualizer(result.slice(), ++counter, 'merge');
-                return result;
-            }
-
-            function leftHalf(array) {
-                let size = Math.floor(array.length / 2);
-                let ptr = 0, i = 0;
-
-                let result = [];
-                while (ptr < size) {
-                    result.push(array[i]);
                     ptr++;
-                    i++;
+                    p1 = ptr;
+                    p2 = p1 - 1;
                 }
-                return result;
+                return array;
             }
 
-            function rightHalf(array) {
-                let size = array.length - (Math.floor(array.length / 2));
-                let ptr = 0, i = Math.floor(array.length / 2);
-
-                let result = [];
-                while (ptr < size) {
-                    result.push(array[i]);
-                    ptr++;
-                    i++;
+            function selectionSort(array) {
+                array = array.slice();
+                let tmp;
+                for (let i = 0; i < array.length - 1; i++) {
+                    for (let j = i + 1; j < array.length; j++) {
+                        if (array[j] < array[i]) {
+                            // swap
+                            tmp = array[i];
+                            array[i] = array[j];
+                            array[j] = tmp;
+                            visualizer(array.slice(), ++counter, 'selection');
+                        }
+                    }
                 }
-                return result;
-            }
-        }
-
-        function quickSort(array) {
-
-            if (array.length <= 1) return array;
-
-            return _quickSort(array.slice(), 0, array.length - 1);
-
-            function _quickSort(array, low, high) {
-                let pivot;
-
-                if (high > low) {
-                    pivot = partition(array, low, high);
-                    _quickSort(array, low, pivot - 1);
-                    _quickSort(array, pivot + 1, high);
-                }
-                return array.slice();
+                return array;
             }
 
-            function partition(array, low, high) {
-                let left, right, target = array[low];
+            function bubbleSort(array) {
+                array = array.slice();
+                let isSorted = false;
+                let stoppingPoint = array.length;
+                while (!isSorted) {
+                    isSorted = true;
+                    for (let current = 0, next = 1; next < stoppingPoint;) {
+                        if (array[current] > array[next]) {
+                            isSorted = false;
+                            let tmp = array[current];
+                            array[current] = array[next];
+                            array[next] = tmp;
+                            visualizer(array.slice(), ++counter, 'bubble');
+                        }
+                        current++;
+                        next++;
+                    }
+                    stoppingPoint--;
+                }
+                return array;
+            }
 
-                left = low;
-                right = high;
+            function mergeSort(array) {
 
-                while (left < right) {
-                    while (array[left] <= target) left++;
-                    while (array[right] > target) right--;
-                    if (left < right)
-                        swap(array, left, right);
+                let n = array.length, a0 = array, a1 = new Array(n),
+                    m, i;
+                for (m = 1; m < n; m <<= 1) {
+                    for (i = 0; i < n; i += m << 1) {
+                        let left = i,
+                            right = Math.min(i + m, n),
+                            end = Math.min(i + (m << 1), n);
+                        merge(a0, a1, left, right, end);
+                    }
+                    i = a0, a0 = a1, a1 = i;
+                }
+                if (array === a1) {
+                    for (let i = 0; i < n; ++i) {
+                        array[i] = a0[i];
+                    }
+                }
+
+                function merge(a0, a1, left, right, end) {
+                    for (let i0 = left, i1 = right; left < end; ++left) {
+                        if (i0 < right && (i1 >= end || a0[i0] <= a0[i1])) {
+                            a1[left] = a0[i0++];
+                        } else {
+                            a1[left] = a0[i1++];
+                        }
+                        visualizer(a1.slice(), ++counter, 'merge');
+                    }
+                }
+            }
+
+            function quickSort(array) {
+
+                return _quickSort(array, 0, array.length - 1);
+
+                function _quickSort(array, left, right) {
                     visualizer(array.slice(), ++counter, 'quick');
+                    if (left < right - 1) {
+                        let pivot = left + right >> 1;
+                        pivot = partition(array, left, right, pivot);
+                        _quickSort(array, left, pivot);
+                        _quickSort(array, pivot + 1, right);
+                    }
                 }
 
-                array[low] = array[right];
-                array[right] = target;
-                return right;
+                function partition(array, left, right, pivot) {
+                    let pivotValue = array[pivot];
+                    swap(array, pivot, --right);
+                    for (let i = left; i < right; ++i) {
+                        if (array[i] < pivotValue) {
+                            swap(array, i, left++);
+                        }
+                    }
+                    swap(array, left, right);
+                    return left;
+                }
+
+                function swap(array, left, right) {
+                    let tmp = array[left];
+                    array[left] = array[right];
+                    array[right] = tmp;
+                }
             }
 
-            function swap(array, indexA, indexB) {
-                let tmp = array[indexA];
-                array[indexA] = array[indexB];
-                array[indexB] = tmp;
+            function use(algorithm) {
+                utils.assertString(algorithm);
+                counter = 0;
+                return {
+                    sort: function (array) {
+                        utils.assertArray(array);
+                        return sortingAlgorithms[algorithm.toLowerCase()](array)
+                    }
+                };
             }
 
-        }
-
-        function use(algorithm) {
-            utils.assertString(algorithm);
-            counter = 0;
             return {
-                sort: function(array) {
-                    utils.assertArray(array);
-                    return sortingAlgorithms[algorithm.toLowerCase()](array)
-                }
+                use: use
             };
-        }
-
-        return {
-            use: use
         };
-    }
 
-    if (typeof define === 'function' && define.amd) {
-        define(function() {
-            return SortAlgorithms;
-        });
-    } else if (typeof module !== 'undefined' && module.exports) {
-        module.exports = SortAlgorithms;
-    } else {
-        window.SortAlgorithms = SortAlgorithms;
-    }
-
-})();
+        exports.default = exports.SortAlgorithms = SortAlgorithms;
+    }));
--- visual.js
+++ visual.js
@@ -1,94 +1,106 @@
-(function() {
-    'use strict';
+;(function (root, factory) {
+    "use strict";
 
-    let sortAlgorithms = new SortAlgorithms(visualizer);
-    let testArray = [];
-    initializeGraphs();
-
-    let inputText = document.getElementById('inputTextData');
-
-    inputText.addEventListener('focusout', function(event) {
-        testArray = getArrayFromInput();
-        initializeGraphs();
-    });
-
-    document
-        .getElementById('startButton')
-        .addEventListener('click', function(event) {
-
-            let arrayInput = getArrayFromInput();
-
-            if (arrayInput.length > 0) testArray = arrayInput.slice();
-
-            initializeGraphs();
-
-            sortAlgorithms
-                .use('insertion')
-                .sort(testArray);
-
-            sortAlgorithms
-                .use('selection')
-                .sort(testArray);
-
-            sortAlgorithms
-                .use('bubble')
-                .sort(testArray);
-
-            sortAlgorithms
-                .use('merge')
-                .sort(testArray);
-
-            sortAlgorithms
-                .use('quick')
-                .sort(testArray);
-        });
-
-    function visualizer(array, counter, algorithm) {
-        (function(arr, _counter, _algorithm) {
-            setTimeout(function() {
-                let dataUpdate = d3
-                    .select(`.${_algorithm}`)
-                    .selectAll('div')
-                    .data(arr);
-
-                let dataEnter = dataUpdate
-                    .enter()
-                    .append('div')
-                    .style('color', 'green');
-
-                dataUpdate
-                    .exit()
-                    .remove();
-
-                dataUpdate
-                    .merge(dataEnter)
-                    .style('height', '10px')
-                    .style('width', d => (d + 1) * 5 + 'px')
-                    .style('background', 'lightgreen')
-                    .style('border', '1px solid black');
-            }, 80 * _counter);
-        })(array.slice(), ++counter, algorithm);
-    }
-
-    function getArrayFromInput() {
-        return inputText.value.trim().split(',').reduce(function(previous, curr) {
-            if (curr.length <= 0) {
-                return previous;
+    if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+        root.document ? factory(root, require('sortalgorithms'), true) : function (w) {
+            if (!w.document) {
+                throw new Error("Visualizer requires a window with a document");
             }
-            if (typeof curr === 'string' && !isNaN(curr)) {
-                previous.push(Number(curr));
-            } else {
-                alert('please enter a valid comma separated numbers as input');
-            }
-            return previous;
-        }, []);
+            return factory(w, require('sortalgorithms'));
+        };
+    } else {
+        factory(root, root.SortAlgorithms);
     }
+}(this || window, function (window, SortAlgorithms, noGlobal) {
+        'use strict';
 
-    function initializeGraphs() {
-        visualizer(testArray.slice(), 0, 'insertion');
-        visualizer(testArray.slice(), 0, 'selection');
-        visualizer(testArray.slice(), 0, 'bubble');
-        visualizer(testArray.slice(), 0, 'merge');
-        visualizer(testArray.slice(), 0, 'quick');
+        const document = window.document;
+
+        function getSortingAlgorithms() {
+            return new SortAlgorithms(visualizer);
+        }
+
+        function getArrayFromInput() {
+            return document.getElementById('inputTextData').value.trim().split(',').reduce(
+                function (previous, curr) {
+                    if (curr.length <= 0) {
+                        return previous;
+                    }
+                    if (typeof curr === 'string' && !isNaN(+curr)) {
+                        previous.push(Number(curr));
+                    } else {
+                        alert('please enter a valid comma separated numbers as input');
+                    }
+                    return previous;
+                }, []);
+        }
+
+        function initializeGraphs(inputArray) {
+            visualizer(inputArray.slice(), 0, 'insertion');
+            visualizer(inputArray.slice(), 0, 'selection');
+            visualizer(inputArray.slice(), 0, 'bubble');
+            visualizer(inputArray.slice(), 0, 'merge');
+            visualizer(inputArray.slice(), 0, 'quick');
+        }
+
+        function visualizer(array, counter, algorithm) {
+            (function (arr, _counter, _algorithm) {
+                setTimeout(function () {
+                    let dataUpdate = d3
+                        .select(`.${_algorithm}`)
+                        .selectAll('div')
+                        .data(arr);
+
+                    let dataEnter = dataUpdate
+                        .enter()
+                        .append('div')
+                        .style('color', 'green');
+
+                    dataUpdate
+                        .exit()
+                        .remove();
+
+                    dataUpdate
+                        .merge(dataEnter)
+                        .style('height', '10px')
+                        .style('width', d => (d + 1) * 5 + 'px')
+                        .style('background', 'lightgreen')
+                        .style('border', '1px solid black');
+                }, 100 * _counter);
+            })(array.slice(), ++counter, algorithm);
+        }
+
+        (function initializeButtonListener() {
+            document
+                .getElementById('startButton')
+                .addEventListener('click', function (event) {
+
+                    let sortAlgorithms = getSortingAlgorithms(),
+                        inputArray = getArrayFromInput() || [];
+
+                    initializeGraphs(inputArray);
+
+                    sortAlgorithms
+                        .use('insertion')
+                        .sort(inputArray);
+
+                    sortAlgorithms
+                        .use('selection')
+                        .sort(inputArray);
+
+                    sortAlgorithms
+                        .use('bubble')
+                        .sort(inputArray);
+
+                    sortAlgorithms
+                        .use('merge')
+                        .sort(inputArray);
+
+                    sortAlgorithms
+                        .use('quick')
+                        .sort(inputArray);
+                });
+        })();
+
     }
-})();
+));
